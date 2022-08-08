464767b5d7fc680e618040f2e0f0585a4c15b4eb
GideonAnderson <103103032+GideonAnderson@users.noreply.github.com>
2022-08-08 14:00:41 +0000

    Add files via upload

---
+++ gka.cpp
@@ -0,0 +1,41 @@
+ /*Gideon Kweeku  Anderson 
+  10976211*/
+ #include <iostream>
+ using namespace std;
+ void prime(int x);
+ int main()
+ {
+int = 0;
+
+  cout<< "any number;"<<endl;
+  cin>>n;
+  prime(n);
+ }
+ void prime(int x)
+ {
+    int sum=0;
+    for(int i=x;i>1 ; i--)
+{
+
+ boolisPrime=true;
+ for (int j=2;j<i;j++)
+ {
+    if (i% j==0)
+  {
+    isPrime=false;
+    j=i;
+  }
+ }
+ if(is prime== true)
+  {
+    cout<<"prime number:"<<i<<",
+    ";
+    sum= sum+ i;
+  }
+}
+cout<<endl<<"sum of thr given"
+prime numbers: "<<sum";
+
+
+Return 0;
+} 
